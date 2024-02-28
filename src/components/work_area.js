fb12309790b6b1b7c1e0f0d033620960fc700405
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-02-27 21:55:48 -0300

    code refact

---
+++ src/components/work_area.js
@@ -0,0 +1,15 @@
+export function workArea() {
+  // Start the list HTML
+  let html = '<div class="keypad">';
+  html += `<h2>Nuova vendita</h2>
+          <span>Vendita</span>
+          <input id="VENDITA" type="text" readonly="" size="5" maxlength="5" value="40" /> 
+          <span>Sala:</span>
+          <input id="SALA" type="text" size="3" maxlength="3" style="" value="0" />
+          <span>Cameriere</span>
+          <select id="CAMERIERE" >&gt;<option value="">Scegli</option><option value="001">001 - Giulia Tudisco</option><option value="GDG">GDG - Giuseppe  De Giosa</option><option value="PT2">PT2 - Paolo Tintori</option><option value="PP">PP - Paulo Paganelli</option></select>
+          <span>Tavolo:</span><input id="TAVOLO" type="text" size="3" maxlength="3" style="" class="numero" value="0" />`;
+  html += "</div>";
+
+  document.getElementById("work_area").innerHTML += html;
+}
--- src/main.js
+++ src/main.js
@@ -22,7 +22,16 @@ document.querySelector('body').innerHTML = `
             <div class="prodotti" id="prodotti"></div>
         </div>
         <div class="right">
-            <div class="work_area" id="work_area">Area di lavoro</div>
+            <div class="work_area" id="work_area">
+                <h2>Nuova vendita</h2>
+                <span>Vendita</span>
+                <input id="VENDITA" type="text" readonly="" size="5" maxlength="5" value="40" /> 
+                <span>Sala:</span>
+                <input id="SALA" type="text" size="3" maxlength="3" style="" value="0" />
+                <span>Cameriere</span>
+                <select id="CAMERIERE" >&gt;<option value="">Scegli</option><option value="001">001 - Giulia Tudisco</option><option value="GDG">GDG - Giuseppe  De Giosa</option><option value="PT2">PT2 - Paolo Tintori</option><option value="PP">PP - Paulo Paganelli</option></select>
+                <span>Tavolo:</span><input id="TAVOLO" type="text" size="3" maxlength="3" style="" class="numero" value="0" />
+            </div>
             <div class="scontrino" id="scontrino"></div>
             <div class="teclado" id="teclado"></div>
         </div>
